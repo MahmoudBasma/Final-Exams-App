28ca2ddb383c0ecd6f5c026c17431bbfcd8d8ea8
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-06 00:40:54 +0300

    filling the lists

--- app/src/main/java/com/example/finalapplication/MainActivity.java
+++ app/src/main/java/com/example/finalapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         coursesList = (ListView) findViewById(R.id.list);
-        ArrayList<String> CRN = new ArrayList<>();
+        ArrayList<String> courses = new ArrayList<>();
         ArrayList<String> links = new ArrayList<>();
 
         SQLiteDatabase db = this.openOrCreateDatabase("finalDB", MODE_PRIVATE, null);
@@ -30,6 +31,26 @@ public class MainActivity extends AppCompatActivity {
                 "VALUES ('CSC498G', 'Mobile Computing', 'https://ionic.io/docs'),"
                  + " ('EDU500','Seminar in Education', 'https://corridorri.com/')," +
                         "('NUT201', 'Nutrition', 'https://www.nutrition.gov/topics/basic-nutrition')");
+
+        //Creating cursor to loop over the results of the query and add them to the lists
+        Cursor c = db.rawQuery("SELECT name, link FROM courses", null);
+
+        int nameIdx = c.getColumnIndex("name");
+        int linkIdx = c.getColumnIndex("link");
+
+        c.moveToFirst();
+
+        //creating the loop and adding the elements to the lists
+
+        while(c != null){
+            String name = c.getString(nameIdx);
+            String link = c.getString(linkIdx);
+
+            courses.add(name);
+            links.add(link);
+        }
+
+
 
 
 
