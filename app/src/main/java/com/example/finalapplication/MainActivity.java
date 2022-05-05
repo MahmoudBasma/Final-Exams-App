6d02558a24bed3ea4ab6e3b011cb4fb5a768af09
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-06 00:23:47 +0300

    declared lists variables

--- app/src/main/java/com/example/finalapplication/MainActivity.java
+++ app/src/main/java/com/example/finalapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.finalapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView coursesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coursesList = (ListView) findViewById(R.id.list);
+        ArrayList<String> CRN = new ArrayList<>();
+        ArrayList<String> date = new ArrayList<>();
+
+
+
+
     }
 }
