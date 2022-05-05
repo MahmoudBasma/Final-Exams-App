28bf53e742214477fda62647ec956af06c3c303a
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-06 00:32:13 +0300

    created db, table and 3 records

--- app/src/main/java/com/example/finalapplication/MainActivity.java
+++ app/src/main/java/com/example/finalapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -17,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         coursesList = (ListView) findViewById(R.id.list);
         ArrayList<String> CRN = new ArrayList<>();
-        ArrayList<String> date = new ArrayList<>();
+        ArrayList<String> links = new ArrayList<>();
+
+        SQLiteDatabase db = this.openOrCreateDatabase("finalDB", MODE_PRIVATE, null);
+        db.execSQL(
+                "CREATE TABLE IF NOT EXISTS courses (CRN VARCHAR, name VARCHAR, link VARCHAR)"
+        );
+
+        db.execSQL(
+                "INSERT INTO links(course_name, course_url) " +
+                "VALUES ('CSC498G', 'Mobile Computing', 'https://ionic.io/docs'),"
+                 + " ('EDU500','Seminar in Education', 'https://corridorri.com/')," +
+                        "('NUT201', 'Nutrition', 'https://www.nutrition.gov/topics/basic-nutrition')");
 
 
 
